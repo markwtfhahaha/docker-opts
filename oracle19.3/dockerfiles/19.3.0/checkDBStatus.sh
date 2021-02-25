3db55a9123b3b3d886dc01f6c27f3f1b065aa63a
markwtfhahaha <mark763000062@gmail.com>
2021-02-25 10:26:26 +0700

    20210225更新

--- oracle19.3/dockerfiles/19.3.0/checkDBStatus.sh
+++ oracle19.3/dockerfiles/19.3.0/checkDBStatus.sh
@@ -14,6 +14,8 @@
 
 ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
 OPEN_MODE="READ WRITE"
+OPEN_MODE_READ="READ ONLY WITH APPLY"
+OPEN_MODE_READ_1="READ ONLY"
 ORAENV_ASK=NO
 source oraenv
 
@@ -21,7 +23,7 @@ source oraenv
 status=`sqlplus -s / as sysdba << EOF
    set heading off;
    set pagesize 0;
-   SELECT DISTINCT open_mode from v\\$database WHERE open_mode = '$OPEN_MODE';
+   SELECT DISTINCT open_mode from v\\$database;
    exit;
 EOF`
    #SELECT DISTINCT open_mode FROM v\\$pdbs WHERE open_mode = '$OPEN_MODE';
@@ -31,6 +33,10 @@ ret=$?
 
 # SQL Plus execution was successful and DB is open
 if [ $ret -eq 0 ] && [ "$status" = "$OPEN_MODE" ]; then
+   exit 0;
+elif [ $ret -eq 0 ] && [ "$status" = "$OPEN_MODE_READ" ]; then
+   exit 0;
+elif [ $ret -eq 0 ] && [ "$status" = "$OPEN_MODE_READ_1" ]; then
    exit 0;
 # DB is not open
 elif [ "$status" != "$OPEN_MODE" ]; then
